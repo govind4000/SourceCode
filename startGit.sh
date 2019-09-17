b1d4b36060e445a2943428fe93b3477264af5062
govindraju.y@hp.com <ygo@g7t00196g.inc.hpicorp.net>
2019-09-17 07:28:13 +0000

    commit

---
+++ startGit.sh
@@ -0,0 +1,8 @@
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github_rsa
+git remote -v
+# git remote add main git@github.azc.ext.hp.com:govindraju-y/sysadmin.git
+# git add <file/folder>
+# git commit -m "string"
+# git push main master
+
